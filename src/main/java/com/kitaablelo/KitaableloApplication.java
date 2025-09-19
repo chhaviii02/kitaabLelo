af22485ad2d540c6f2fe4e6e320402ac0c92c617
Chhavi Sahu <chhavisahu734@gmail.com>
2025-09-19 15:39:24 +0530

    spring setup

---
+++ src/main/java/com/kitaablelo/KitaableloApplication.java
@@ -0,0 +1,13 @@
+package com.kitaablelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitaableloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitaableloApplication.class, args);
+	}
+
+}
